1a996b20b50f47f919221b7cf718889bd7b81b66
Owen Cliffe <owen.cliffe@oracle.com>
2018-05-14 13:19:04 -0700

    Start integrating trigger calls

--- api/const.go
+++ api/const.go
@@ -5,6 +5,7 @@ const (
 	AppName string = "app_name"
 	AppID   string = "app_id"
 	Path    string = "path"
+	Func    string = "func"
 
 	// Gin  URL template parameters
 	ParamAppID     string = "appID"
@@ -12,5 +13,8 @@ const (
 	ParamRouteName string = "route"
 	ParamTriggerID string = "triggerID"
 	ParamCallID    string = "call"
-	ParamFnID      string = "fnID"
+	ParamFnID      string = "fnId"
+
+	//
+	ParamSource string = "source"
 )
